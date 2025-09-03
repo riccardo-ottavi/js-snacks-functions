1a8fee713df42f6abdec6a35bfa4e41f7b709350
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-03 14:38:47 +0200

    risolvi es 1

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,15 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const userName = 'Mario';
-console.log(userName);
+
 
 // Dichiara la funzione qui.
-
+function sayHello(name) {
+    return "Ciao " + name;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let helloString = sayHello(userName);
+console.log(helloString);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
