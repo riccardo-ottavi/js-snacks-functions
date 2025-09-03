5d2c9d29826b6b97bb5c37ab9953b91796b4819b
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-03 15:41:46 +0200

    risolvi bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -8,12 +8,25 @@ e buonasera se è sera (oltre le 17)
 const name = 'Mario';
 const now = new Date();
 const currentTime = now.toLocaleTimeString();
+const hour = now.getHours();;
 
 
 // Dichiara la funzione qui.
+function gMorningOrNight(name) {
+    if (hour > 13 && hour < 17) {
+        return "Buon pomeriggio " + name;
+    }else if(hour < 13 ) {
+        return "Buongiorno " + name;
+    }else if (hour > 17) {
+        return "Buonasera " + name;
+    }
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+let helloString = gMorningOrNight(name);
+console.log(helloString);
+
 
 
 
