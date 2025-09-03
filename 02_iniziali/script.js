c75fb9e36853fc7acbbb8fa69c4c62118ab9a9f1
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-03 14:55:29 +0200

    risolvi es 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function firstLetterCreate(array) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let str = array[i];
+        newArray.push(str.substr(0,1));
+    }
+    return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let firstLetterArray = firstLetterCreate(names);
+console.log(firstLetterArray);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
