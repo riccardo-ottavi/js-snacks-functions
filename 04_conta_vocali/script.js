641b921564a8b6c6938ce8b561082302f5bb05c7
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-03 15:10:53 +0200

    risolvi es 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,23 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+const vowels = ["a" ,"e" ,"i" ,"o" ,"u"];
 
 // Dichiara la funzione qui.
-
+function countVowels(string) {
+    let counter = 0;
+    for(let i = 0; i < string.length; i++) {
+        if(vowels.includes(string[i])){
+            counter ++;
+        }
+        
+    }
+    return counter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+vowelsCounter = countVowels(word);
+console.log(vowelsCounter);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
