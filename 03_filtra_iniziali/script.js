4bb9d63de32966572632539f547cbfd7ea9af525
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-03 15:03:09 +0200

    risolvi es 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,20 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function searchFirstLetter(array, letter) {
+    let newArray =[];
+    for (let i= 0; i < array.length; i++) {
+        let firstLetter = array[i].substr(0,1);
+        if (firstLetter === letter) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let firstLetterArray = searchFirstLetter(names, "A");
+console.log(firstLetterArray);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
